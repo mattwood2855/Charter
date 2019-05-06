bc1d79c790b11d95871550162caa560855ef45d7
Matthew Wood <mattwood2855@gmail.com>
2019-05-06 16:47:47 -0700

    init

---
+++ Charter/Charter/ViewModels/NewUserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Charter.ViewModels
+{
+    public interface INewUser
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+
+        void TrySave();
+    }
+
+    public class NewUserViewModel : INewUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public IEnumerable<string> Rules => new List<string>()
+        {
+            "/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/", // Checks at least one number, one letter, only alphanumeric chars
+            "/^[a-zA-Z0-9]{5,12}", // Checks the lenth is between 5 and 12
+        };
+
+        public void TrySave ()
+        {
+
+        }
+    }
+}
